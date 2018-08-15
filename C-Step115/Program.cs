290d738b66e1e60cc935ce22a354483a6eff09db
ckaatz <39869540+ckaatz@users.noreply.github.com>
2018-08-14 21:33:15 -0500

    Add project files.

---
+++ C-Step115/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Step115
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MathClass NewClass = new MathClass;
+            Console.WriteLine("Pick a number:");
+            int UserInput1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("You may enter a second number if you wish:");
+            int UserInput2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(NewClass.MathOperation(UserInput1, UserInput2));
+            Console.ReadLine();
+        }
+    }
+
+    public class MathClass
+    {
+        public int MathOperation(int Num1, int Num2 = 1)
+        {
+            return Num1 * Num2;
+        }
+    }
+}
---
+++ C-Step115/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("C-Step115")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Hewlett-Packard")]
+[assembly: AssemblyProduct("C-Step115")]
+[assembly: AssemblyCopyright("Copyright © Hewlett-Packard 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("9d8802b1-ce22-4786-91c2-414d8d4934b4")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
